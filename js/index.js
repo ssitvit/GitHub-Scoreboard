ab8d8d6707b499cbef4af2073b5f855dd8860134
Gunjan Raj Tiwari <im.gunjan1@gmail.com>
2020-12-23 18:38:14 +0545

    github api and all js part

--- js/index.js
+++ js/index.js
@@ -1,3 +1,33 @@
+const usersAPI = "https://api.github.com/users/"
+async function getUserData(uid){
+    let cardData = {}
+    try {
+        const userData = await (await fetch(usersAPI+uid)).json()
+        if(userData.message){
+            alert(userData.message)
+            return false
+        }
+        cardData.name = userData.login;
+        cardData.image = userData.avatar_url;
+        cardData.url = userData.html_url;
+        cardData.followers = userData.followers;
+        const repos = await (await fetch(userData.repos_url)).json()
+        cardData.repos = repos.length;
+        let stars = 0;
+        let forks = 0;
+        repos.forEach(repo => {
+            forks+= repo.forks_count;
+            stars+= repo.stargazers_count;
+        })
+        cardData.forks= forks;
+        cardData.stars= stars;
+        return cardData;
+    }catch(e){
+        console.log(e)
+    }
+}
+getUserData('gunjanrajtiwari')
+
 function changeTheme(code){
     document.documentElement.style
     .setProperty('--neon', code);
@@ -15,10 +45,41 @@ function comparePage(){
     $('.container').load('compare.html')
 }
 
-function getScore(form){
-    alert('You entered: '+form.uid.value)
+function displayCards(data){
+    const cards = document.getElementById('myCard')
+    cards.innerHTML='';
+    for(user of data){
+        let total = user.repos+user.forks+2*user.stars+2*user.followers;
+        let card = `
+        <div class="profile-card">
+            <h2 class="card-title">${user.name}</h2>
+            <div class="img-div"><img class="profile-pic" src=${user.image} alt="Photo"></div>
+            <div class="github-info">
+                <p>Total Followers: <span>${user.followers}</span></p>
+                <p>Total Stars: <span>${user.stars}</span></p>
+                <p>Total Forks: <span>${user.forks}</span></p>
+                <p>Public Repos: <span>${user.repos}</span></p>
+            </div>
+            <h2 class="final-score">Your score is: ${total}</h2>
+            <button onclick="window.open('${user.url}','_blank')" class="btn-card">Profile</button>
+        </div>
+        `
+        cards.innerHTML+=card;
+    }
 }
 
-function compareScore(form){
-    alert('You entered :'+form.uid1.value+' and '+form.uid2.value)
+async function getScore(form){
+    event.preventDefault()
+    const userData = await getUserData(form.uid.value)
+    if(userData){
+        displayCards([userData]);
+    }
+}
+
+async function compareScore(form){
+    event.preventDefault()
+    const [user1,user2] = await Promise.all([getUserData(form.uid1.value),getUserData(form.uid2.value)])
+    if(user1 && user2){
+        displayCards([user1,user2])
+    }
 }
