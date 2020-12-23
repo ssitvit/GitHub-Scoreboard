8a132b401360fef00937d48abc543ebd056f2f7c
Gunjan Raj Tiwari <im.gunjan1@gmail.com>
2020-12-23 12:26:48 +0545

    single page

--- js/index.js
+++ js/index.js
@@ -2,3 +2,23 @@ function changeTheme(code){
     document.documentElement.style
     .setProperty('--neon', code);
 }
+
+function homePage(){
+    $('.container').load('home.html')
+}
+
+function checkPage(){
+    $('.container').load('check.html')
+}
+
+function comparePage(){
+    $('.container').load('compare.html')
+}
+
+function getScore(){
+    alert('Your Score is very good.')
+}
+
+function compareScore(){
+    alert('Your Score is compared')
+}
